8be19aed6bbc25b8456f58f7ab194e4e5f06be17
Elly C <elly.chan.mda@gmail.com>
2018-10-31 23:45:04 +0000

    17 not finished

--- boris_bike/lib/docking_station.rb
+++ boris_bike/lib/docking_station.rb
@@ -1,23 +1,41 @@
 require_relative "bike"
 
 class DockingStation
+	DEFAULT_CAPACITY = 20
 
-	attr_reader :bike
+	attr_reader :bikes, :capacity
+
+	def initialize(capacity = DEFAULT_CAPACITY)
+		@bikes = []
+		@capacity = capacity
+	end
+	#attr_reader :bike
 	#def bike
 	#	@abike # when a bike is docked, have a bike in station
 	#end
 	#method that used to only call the initialized var., can use atr_reader
 
-	def release_bike
-		Bike.new
-	end
+	def release_bike 
+    #Bike.new #this release a new bike, remove at step12 becoz only release if we have bike
+		fail "no bike available" if empty?
+    @bikes.pop # if ary contains, (pop) remove the last elem. in ary
+  end
 
 	
 	def dock(bike) # this acts like initilize
-		@bike = bike
+		fail "docking station full" if full?
+		@bikes << bike
 	end
 
+private
 
+	def full?
+  	@bikes.count == @capacity
+  end
+
+  def empty?
+  	@bikes.empty?
+  end
 
 end
 
--- boris_bike/spec/docking_station_spec.rb
+++ boris_bike/spec/docking_station_spec.rb
@@ -4,19 +4,37 @@ describe DockingStation do
 
 	it { should respond_to(:release_bike)}
 
-	it "able to call working? and return true" do
-		station = DockingStation.new
-		bike = station.release_bike
-		expect(bike.working?).to eq true
-	end
+	#it "able to call working? and return true" do
+	#	station = DockingStation.new
+	#	bike = station.release_bike
+	#	expect(bike.working?).to eq true
+	#end
 
 	it { is_expected.to respond_to(:dock).with(1).argument }
 
-	it "able to dock a bike then stores it in" do
-		something = Bike.new # use "DockingStation.new" works too, why Bike?
-		subject.dock(something) # subject: -> x = DockingStation.new 
-		expect(subject.bike).to eq something
-	end
-	# -- the above test removed was because def bike is removed. attr_reader :bike is used instead
+	describe "#release_bike" do # nested because describing behaviour specific to a particular method
+		it "able to dock a bike then stores it in" do
+			something = Bike.new # use "DockingStation.new" works, why pass Bike class?
+			subject.dock(something) # subject: -> x = DockingStation.new 
+			expect(subject.bikes).to eq [something] #the passed arg shoud be in ary
+		end
+
+		it "show error when no bike is docked in station" do
+    	expect {subject.release_bike}.to raise_error "no bike available"
+ 	 	end
+ 	end #nesting end
+
+ 		it "if already have bike, return error when dock bike" do
+ 			DockingStation::DEFAULT_CAPACITY.times do
+ 				subject.dock(Bike.new) # either Bike or DockingStation, this is a var that runs in the class not method
+ 			end # use "DockingStation.new" works, why pass Bike class?
+    	expect {subject.dock(Bike.new)}.to raise_error "docking station full"
+ 	 	end
+ 	 	#[ something = Bike.new; subject.dock(something) ] = subject.dock(Bike.new)
+
+
+ 	 	it "set capacity when use DockingStation.new, default if no input" do
+ 	 		expect(subject.capacity).to eq DockingStation::DEFAULT_CAPACITY
+ 	 	end
 
 end
